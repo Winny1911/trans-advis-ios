89ecf581bd6142c9fa22e20b3f1033620cb43a04
Roberto Luiz Veiga Junior <roberto@MacBook-Pro-de-Roberto.local>
2023-03-01 18:15:11 -0300

    model

--- TA/Controllers/TA Contrator/ProfileTabbarVC/InvitationsTabBar/PlaceBidModel.swift
+++ TA/Controllers/TA Contrator/ProfileTabbarVC/InvitationsTabBar/PlaceBidModel.swift
@@ -14,11 +14,25 @@ class PlaceBidModel {
     var endDate: String = ""
     var description: String = ""
     var arrOfFilesCount :Int = 0
+    var homeOwnerFirst: String = ""
+    var homeOwnerSecond: String = ""
+    var streetAddress: String = ""
+    var mailingAddress: String = ""
+    var cellPhone: String = ""
+    var email: String = ""
+    var hoa: String = ""
+    var permit: String = ""
+    var insurance: String = ""
+    var claimNeumber: String = ""
+    var insFullyApproved: String = ""
+    var insPartialApproved: String = ""
+    var retail: String = ""
+    var RetailWDepreciation: String = ""
     
     init() {
     }
     
-    init(bidAmount: String, amountReceivable: String, startDate: String, endDate: String, description: String, arrOfFilesCount:Int) {
+    init(bidAmount: String, amountReceivable: String, startDate: String, endDate: String, description: String, arrOfFilesCount:Int, homeOwnerFirst: String, homeOwnerSecond: String, streetAddress: String, mailingAddress: String, cellPhone: String, email: String, hoa: String, permit: String, insurance: String, claimNeumber: String, insFullyApproved: String, insPartialApproved: String, retail: String, retailWDepreciation: String, mainDwellingRoof: String, shedSQ: String, decking: String, flatRoofSQ: String, total: String, deducible: String) {
         self.bidAmount = bidAmount
         self.amountReceivable = amountReceivable
         self.startDate = startDate
